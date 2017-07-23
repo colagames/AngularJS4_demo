cf9d1731d3ef5eeaaae19ec2daa82045c10796fc
yihsin <cheriish2001@hotmail.com>
2017-07-23 16:02:40 +0800

    使用 傳出事件 – @Output() myEvent = new EventEmitter<any>(); – this.myEvent.emit(data); – 在外層元件請記得用「事件繫結」來接收傳出的資料 • 在 Template 中使用 $event 代表子元件傳出的資料
    來做到DELETE

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -11,7 +11,14 @@ export class ArticleComponent implements OnInit {
   item;
   constructor() { }
 
+  @Output()
+  delete = new EventEmitter<any>();
+
   ngOnInit() {
+  }
+
+  doDelete() {
+    this.delete.emit(this.item);
   }
 
 }
