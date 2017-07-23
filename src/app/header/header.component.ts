d5529169057f6c853b89b940f56874b87aa2b114
yihsin <cheriish2001@hotmail.com>
2017-07-23 12:01:28 +0800

    練習用其他的屬性繫結方式

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent implements OnInit {
 
   siteName = '我的部落格';
   siteUrl = 'https://docs.google.com/forms/d/e/1FAIpQLScI9zHZ4n5CXdN8jKanIr1UDkd5axr7o7ngrSv-_2Ydpj_2Pw/viewform';
-
+  siteSubTitle = '記載著 今天所有的Demo練習 ';
   constructor() { }
 
   ngOnInit() {
