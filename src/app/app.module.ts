57ef61511332609eed760b34ddc931405acce2a5
yihsin <cheriish2001@hotmail.com>
2017-07-23 16:39:15 +0800

    下ng g p mask ，建立新Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { FormsModule } from '@angular/forms';
-import { ArticleComponent } from './article/article.component'; // 註冊Forms 才可以使用 [(ngModule)]
+import { ArticleComponent } from './article/article.component';
+import { MaskPipe } from './mask.pipe'; // 註冊Forms 才可以使用 [(ngModule)]
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ArticleComponent
+    ArticleComponent,
+    MaskPipe
   ],
   imports: [
     BrowserModule,
